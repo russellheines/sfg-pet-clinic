e1e673471bdd21ab6b6b7db6f0eac2bc4cb833b6
Russell Heines <rheines@patientkeeper.com>
2020-04-16 00:18:39 -0400

    Update Thymeleaf templates to list owners and vets

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfVets() {
-        return "vets/index";
+    public String listOfVets(Model model) {
+
+        model.addAttribute("owners", ownerService.finalAll());
+
+        return "owners/index";
     }
 }
