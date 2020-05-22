cf6c81932764b893887ced122e36e60a313ad349
Russell Heines <rheines@patientkeeper.com>
2020-05-22 13:33:38 -0400

    Add specialty service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -31,10 +31,10 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
     }
 
     @Override
-    public Owner save(Owner owner) {
-        if (owner != null) {
-            if (owner.getPets() != null) {
-                owner.getPets().forEach(pet -> {
+    public Owner save(Owner object) {
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
                   if (pet.getPetType() != null) {
                       if (pet.getPetType().getId() == null) {
                           pet.setPetType(petTypeService.save(pet.getPetType()));
@@ -52,7 +52,7 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
             }
         }
 
-        return super.save(owner);
+        return super.save(object);
     }
 
     @Override
